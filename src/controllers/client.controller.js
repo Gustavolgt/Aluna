4359f2616967a1ed130f4aca2c82b58ae401aa4c
Gustavolgt <gustavocms16@gmail.com>
2022-06-08 08:07:13 -0300

    cliente. contr

--- src/controllers/client.controller.js
+++ src/controllers/client.controller.js
@@ -4,7 +4,10 @@ import { prisma } from '../database/prisma.ts'
 const getClient = async (id, res) => {
 	const resp = await prisma.clients.findMany({
 		where: {
-			number: id
+			number: {
+				contains: id
+			},
+			user_id: 'cl29fmf4e005809l82h7oqqbm'
 		}
 	})
 	res.status(200).send(resp[0])
